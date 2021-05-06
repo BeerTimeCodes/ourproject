71a6b3a63241f25825edc4cd89e2362d15f39d6a
Niek506 <50707977+Niek506@users.noreply.github.com>
2021-05-06 22:37:52 +0200

    Add files via upload

---
+++ commands/creator.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name:'creator',
+    description:'tells you who created this awesome bot',
+    execute(client, message, args){
+        message.channel.send('<@490940484202790943> and <@797180081353130045> created me!!')
+    }
+
+}
---
+++ commands/ping.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'ping',
+    description: "this is a ping command!",
+    execute(client, message, args){
+        message.channel.send('pong');
+    }
+
+}
